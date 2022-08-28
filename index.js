4df0a7c483a9145e4e7fb4e4dfc91ec7f1b470cb
Kazumi <77766586+Akanzuu@users.noreply.github.com>
2022-08-28 22:46:12 +0700

    first

--- index.js
+++ index.js
@@ -25,14 +25,13 @@ app.post('/arkose-proxy', (req, res) => {
     const blob = req.query.data.blob
     const proxy = req.query.proxy.split(":")
     axios({
-        method: 'get',
-        url: 'https://roblox-api.arkoselabs.com/fc/gt2/public_key/476068BF-9607-4799-B53D-966BE98E2B81',
+        method: 'POST',
+        url: `https://roblox-api.arkoselabs.com/fc/gt2/public_key/476068BF-9607-4799-B53D-966BE98E2B81?bda=${bda}&public_key=${public_key}&userbrowser=${userbrowser}&language=${language}&rnd=${rnd}&data[blob]=${blob}`,
         headers: {
-            'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'content-type': 'application/json',
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36',
-            'x-requested-with': 'XMLHttpRequest',
-            'Referer': 'https://www.roblox.com/',
-            'Origin': 'https://www.roblox.com',
+            'referer': 'https://www.roblox.com/',
+            'origin': 'https://www.roblox.com',
         },
         responseType: 'json',
         proxy: {
@@ -44,19 +43,19 @@ app.post('/arkose-proxy', (req, res) => {
               password: '45vi42o9ct0ww'
             }
           },
-          params: {
-            bda: encodeURI(bda),
-            public_key: public_key,
-            site: "https://www.roblox.com" ,
-            userbrowser: encodeURI(userbrowser),
-            language: language,
-            rnd: rnd,
-            data: {
-                blob: encodeURI(blob) ,
-            }
-        }
+        //   params: {
+        //     bda: encodeURI(bda),
+        //     public_key: public_key,
+        //     site: "https://www.roblox.com" ,
+        //     userbrowser: encodeURI(userbrowser),
+        //     language: language,
+        //     rnd: rnd,
+        //     data: {
+        //         blob: blob
+        //     },
+        // }
       }).then(function (response) {
-          res.send(response.data)
+          console.log(response)
         }).catch(function (error) {
             if (error.response) {
               res.send(error.response.status);
